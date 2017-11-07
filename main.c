1f8a5031b6776a60c485be8168610f6790cd12cb
karmakararunava <arunavo111@gmail.com>
2017-11-07 22:30:31 +0530

    appropriate output obtained

--- main.c
+++ main.c
@@ -2,21 +2,70 @@
 #include <stdlib.h>
 #include <string.h>
 
-//extern int *c;
-
-//int *fun_stringTest ();
+int fun_stringTest ();
 int fun_stringTest1 ();
-//int fun_numTest ();
+int fun_numTest ();
 //int sum ();
 
 int main () {
 	//printf ("Sum = %d\n", *c);
-	//fun_stringTest ();
-	//fun_numTest ();
+	fun_stringTest ();
 	fun_stringTest1 ();
-	//printf ("%c\n", fun_stringTest());
+	fun_numTest ();
 	return EXIT_SUCCESS;
 }
+
+
+/**/
+int fun_stringTest () {
+	char *buffer = "Hello, World!!!"; 
+	char *testBuffer = malloc (sizeof (buffer));
+	testBuffer = buffer;
+
+	// output to console
+	printf ("1. %s\n", buffer); // correct
+	
+	//printf ("2. %s\n", *buffer); // incorrect!! - segmentation fault (core dumped)
+	
+	//printf ("3. %c\n", buffer); // incorrect - output:- (?)
+	
+	//printf ("4. %c\n", *buffer); // incorrect - output:- H
+	
+	printf ("5. %s\n", testBuffer); // correct
+	
+	//printf ("6. %s\n", *testBuffer); // incorrect!! - segmentation fault (core dumped)
+	
+	//printf ("7. %c\n", testBuffer); // incorrect - output:- (?)
+	
+	//printf ("8. %c\n", *testBuffer); // incorrect - output:- H
+	
+	//free (testBuffer);
+	return 0; 
+}
+/**/
+
+
+/**/
+int fun_stringTest1 () {
+	char message[] = "Arunava here...";
+	char *buffer = malloc(sizeof (message));
+	buffer = message;
+	printf ("%s\n", buffer);
+	return 0;
+}
+/**/
+
+
+/**/
+int fun_numTest () {
+	int num = 007;
+	int *numPointer;
+	numPointer = &num;
+	printf ("%.3u\n", *numPointer);
+	return 0;
+}
+/**/
+
 
 /*
 int sum () {
@@ -25,39 +74,5 @@ int sum () {
 	*b = 8;
 	*c = *a + *b;
 	return 0;
-}
-*/
-
-/*
-int fun_numTest () {
-	int num = 31;
-	int *numPointer;
-	numPointer = &num;
-	printf ("%u\n", *numPointer);
-	//printf ("%c", *passBuffer);
-	return 0;
-}
-*/
-
-/**/
-int fun_stringTest1 () {
-	char message[] = "Hello, World!!!";
-	char *buffer;
-	buffer = message;
-	printf ("%c\n", *buffer);
-	return 0;
-}
-/**/
-
-/*
-int *fun_stringTest () {
-	char *buffer;
-	buffer = "Hello, World!!!";
-	//char *passBuffer;
-	//passBuffer = &buffer;
-	//memcpy (*passBuffer, *buffer, 15);
-	printf ("%c\n", *buffer);
-	//printf ("%c", *passBuffer);
-	return (buffer);
 }
 */
